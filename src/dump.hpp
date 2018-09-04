3cdfcfdb7e5c521f5f744bb904ca8a578f98d628
Fangrui Song <i@maskray.me>
2018-09-04 00:08:06 -0700

    beautify dump.hpp

--- src/dump.hpp
+++ src/dump.hpp
@@ -13,11 +13,11 @@ struct Trunk {
 };
 
 // Helper function to print branches of the binary tree
-void showTrunks(std::ostream& os, Trunk* p) {
+void showTrunks(std::ostream& os, Trunk* p, int first = 1) {
     if (p == nullptr)
         return;
-    showTrunks(os, p->prev);
-    os << p->str;
+    showTrunks(os, p->prev, 0);
+    os << p->str.substr(0, p->str.size() - first);
 }
 
 template <typename T>
@@ -28,7 +28,7 @@ void dump(std::ostream& os, T* root, Trunk* prev = nullptr, bool left = true) {
     Trunk* trunk = new Trunk(prev, prev_str);
     dump(os, root->left, trunk, true);
     if (!prev)
-        trunk->str = "---";
+        trunk->str = "----";
     else if (left) {
         trunk->str = ".---";
         prev_str = "   |";
